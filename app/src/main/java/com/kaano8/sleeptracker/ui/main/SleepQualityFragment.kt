1a05b9d08d5298786d99a6fd3cc2aa30b87db06a
aayum <aayushimathur95@gmail.com>
2021-08-10 01:33:18 +0530

    ui_end: basic ui

--- app/src/main/java/com/kaano8/sleeptracker/ui/main/SleepQualityFragment.kt
+++ app/src/main/java/com/kaano8/sleeptracker/ui/main/SleepQualityFragment.kt
@@ -1,5 +1,6 @@
 package com.kaano8.sleeptracker.ui.main
 
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,33 +8,34 @@ import android.view.View
 import android.view.ViewGroup
 import com.kaano8.sleeptracker.DashboardActivity
 import com.kaano8.sleeptracker.R
+import com.kaano8.sleeptracker.databinding.SleepQualityFragmentBinding
 import com.kaano8.sleeptracker.databinding.SleepTrackerFragmentBinding
 
-class SleepTrackerFragment : Fragment() {
+class SleepQualityFragment : Fragment() {
 
-    private lateinit var _binding: SleepTrackerFragmentBinding
+    private lateinit var _binding: SleepQualityFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = SleepTrackerFragmentBinding.inflate(inflater, container, false)
+        _binding = SleepQualityFragmentBinding.inflate(inflater, container, false)
         return _binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       _binding.stopButton.setOnClickListener {
-           (activity as DashboardActivity).supportFragmentManager.beginTransaction()
-               .replace(R.id.container, SleepQualityFragment.newInstance())
-               .commitNow()
-       }
+        _binding.qualityFiveImage.setOnClickListener {
+            (activity as DashboardActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.container, SleepTrackerFragment.newInstance())
+                .commitNow()
+        }
 
     }
 
     companion object {
-        fun newInstance() = SleepTrackerFragment()
+        fun newInstance() = SleepQualityFragment()
     }
 
 }
---
+++ app/src/main/java/com/kaano8/sleeptracker/ui/main/SleepQualityViewModel.kt
@@ -0,0 +1,7 @@
+package com.kaano8.sleeptracker.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class SleepQualityViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
