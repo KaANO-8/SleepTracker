4dababd0e3f10f5340a1101bf654b0837ad4a712
aayum <aayushimathur95@gmail.com>
2021-08-12 19:32:06 +0530

    ui_end: listview

--- app/src/main/java/com/kaano8/sleeptracker/ui/main/SleepTrackerFragment.kt
+++ app/src/main/java/com/kaano8/sleeptracker/ui/main/SleepTrackerFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.kaano8.sleeptracker.DashboardActivity
 import com.kaano8.sleeptracker.R
 import com.kaano8.sleeptracker.databinding.SleepTrackerFragmentBinding
@@ -24,11 +25,42 @@ class SleepTrackerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val arrayAdapter: ArrayAdapter<*>
+        val list = arrayOf(
+            getString(R.string.sleep_data)
+        )
+
+        arrayAdapter = ArrayAdapter(
+            context as DashboardActivity,
+            android.R.layout.simple_list_item_1, list
+        )
+        _binding.list.adapter = arrayAdapter
+
+        _binding.startButton.setOnClickListener {
+            _binding.clearButton.isEnabled = false
+            _binding.startButton.isEnabled = false
+            showLoader()
+        }
+
        _binding.stopButton.setOnClickListener {
+           hideLoader()
            (activity as DashboardActivity).supportFragmentManager.beginTransaction()
                .replace(R.id.container, SleepQualityFragment.newInstance())
                .commitNow()
        }
+
+        _binding.clearButton.setOnClickListener {
+
+        }
+
+    }
+
+    private fun showLoader() {
+        _binding.loader.visibility = View.VISIBLE
+    }
+
+    private fun hideLoader() {
+        _binding.loader.visibility = View.GONE
 
     }
 
